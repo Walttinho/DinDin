c4a81e85a9d06ca0bb420abf9b30f081f101b726
Walttinho <walter.netto@live.com>
2023-10-02 22:44:08 -0300

    fix: response

--- src/controladores/categorias.controladores.js
+++ src/controladores/categorias.controladores.js
@@ -2,7 +2,7 @@ const pool = require('../conexão')
 
 const buscarCategorias = async(req, res)=>{
 const categorias = await pool.query('select * from categorias')
-res.status(200).json(categorias)
+res.status(200).json(categorias.rows)
 }
 
 module.exports = buscarCategorias
