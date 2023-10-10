c8f99693091b3e4fa15b581962bb2ce9077654b5
Walttinho <walter.netto@live.com>
2023-10-10 19:12:11 -0300

    add: swagger doc

---
+++ src/router/swagger.route.js
@@ -0,0 +1,12 @@
+const swaggerRouter = require('express').Router()
+
+const swaggerUi = require('swagger-ui-express')
+
+const swaggerDocument = require("../swagger.json");
+
+
+swaggerRouter.use("/", swaggerUi.serve);
+swaggerRouter.get("/", swaggerUi.setup(swaggerDocument));
+
+
+module.exports = swaggerRouter;
