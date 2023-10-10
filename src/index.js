f8afd3bd85b818ed9e2a6e00a9f852faf3b5132f
Walttinho <walter.netto@live.com>
2023-10-10 19:13:03 -0300

    fix: ajuste mensagem servidor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
-const rotas = require("./rotas");
+const router = require("./router/index.router");
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
-app.use(rotas);
+app.use(router);
+const port = process.env.PORT || 3000;
 
-app.listen(3000);
+app
+  .listen(port, () => {
+    console.log(`O Servidor está rodando na porta: ${port}`);
+  })
+  .on("error", (err) => {
+    process.once("SIGUSR2", () => {
+      process.kill(process.pid, "SIGUSR2");
+    });
+  });
