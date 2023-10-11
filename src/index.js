7ccfb31c61f6b8b8dc92e0798f3c6e60613d3590
Walttinho <walter.netto@live.com>
2023-10-10 23:57:00 -0300

    add: cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const cors = require('cors')
 const router = require("./router/index.router");
 require("dotenv").config();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(router);
 const port = process.env.PORT || 3000;
