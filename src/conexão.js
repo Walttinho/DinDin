ca2c293d00cbb9bc81e97339aa727c89bc12867d
Walttinho <walter.netto@live.com>
2023-10-10 22:49:45 -0300

    fix: conexao por string

--- src/conexão.js
+++ src/conexão.js
@@ -2,11 +2,7 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  connectionString: process.env.DB_URL
 });
 
 module.exports = pool;
